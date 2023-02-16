f5f631572d2f7a5cc26ff72c28426a4fac307320
atomfaster@gmail.com <atomfaster@gmail.com>
2023-02-16 17:56:38 +0700

    commit history detail page

--- src/views/GPSTrackingPage-submenu/HistoryTruck.vue
+++ src/views/GPSTrackingPage-submenu/HistoryTruck.vue
@@ -1,32 +1,46 @@
 <template>
     <SubMenu />
     <!-- {{ AllEvents }} -->
-    <div v-for="index in 10" :key="index">
-        <div v-for="allevents in AllEvents[index]" :key="allevents.registration">
-        <!-- Show only match w/ props -->
-        <div v-if="allevents.registration === id">
-            <p>License plate: {{ allevents.registration }}</p>
-            <p>Start</p>
-            <p>Start time: {{ allevents.start_timestamp }}</p>
-            <p>Start Lacation: {{ allevents.start_location }}</p>
-            <br>
-            <p>End</p>
-            <p>End time: {{ allevents.end_timestamp }}</p>
-            <p>End Lacation: {{ allevents.end_location }}</p>
-            <hr />
-            <p>Duration: {{ allevents.trip_duration }}</p>
-            <hr />
-            <br>
-        </div>
+    <div class="font-sans font-bold pt-8 pb-3 text-3xl text-gray-400">Recent Activity</div>
+            <div>({{ id }})</div>
+            <div class="inline-flex justify-center items-center w-full">
+                <hr class="my-6 w-3/5 h-1 bg-gray-400 border-0">
+            </div>
+    <div class="grid grid-cols-4 gap-4 py-5 font-bold">
+        <div>Date/Time</div>
+        <div>Location</div>
+        <div>Driver</div>
+        <div>Duration</div>
+    </div>
+    <div>
+        <div v-for="index in 10" :key="index">
+            <div v-for="allevents in AllEvents[index]" :key="allevents.registration">
+                <!-- Show only match w/ props -->
+                <div v-if="allevents.registration === id">
+                    <div class="grid grid-cols-4 gap-4">
+                        <div>
+                            <p :style="{'background': '#323948'}">Start time: {{ FormData(allevents.start_timestamp) }}</p>
+                            <p>End time: {{ FormData(allevents.end_timestamp) }}</p>
+                            <br>
+                        </div>
+                        <div>
+                            <p :style="{'background': '#323948'}">{{ allevents.start_location }}</p>
+                            <p>{{ allevents.end_location }}</p>
+                            <br>
+                        </div>
+                        <div>
+                            <p :style="{'background': '#323948'}">{{ allevents.driver_name }}</p>
+                            <br>
+                        </div>
+                        <div>
+                            <p :style="{'background': '#323948'}">{{ allevents.trip_duration }}</p>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    
-    <!-- <div class="flex flex-col flex-nowrap justify-end w-2 h-52 bg-gray-200 rounded-full overflow-hidden dark:bg-gray-700">
-        <div class="bg-blue-500 overflow-hidden" role="progressbar" style="height: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-        <div class="bg-blue-500 overflow-hidden" role="progressbar" style="height: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-        <div class="bg-blue-500 overflow-hidden" role="progressbar" style="height: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-        <div class="bg-blue-500 overflow-hidden" role="progressbar" style="height: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-    </div> -->
     
 </template>
 <script>
@@ -49,12 +63,18 @@ import SubMenu from '../../components/SubMenu.vue';
                 response => {
                     for(let i=0 ; i<10 ; i++){
                         this.AllEvents.push(response.data[i].data)
-                    //     for(let x=0 ; x<10 ; x++){
-                    //         this.AllEvents.push(response.data[i].data)
-                    //     }
                     }
                     console.log(this.AllEvents)
                 })
+            },
+            FormData(data) {
+                let dateRegex = /^\d{4}-\d{2}-\d{2}/
+                let timeRegex = /\d{2}:\d{2}:\d{2}/
+
+                let dateMatch = data.match(dateRegex);
+                let timeMatch = data.match(timeRegex);
+
+                return `${dateMatch[0]} @ ${timeMatch[0]}`
             }
         },
         created() {
