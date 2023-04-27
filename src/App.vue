a1a4cea0ce503d9a4aea79ee2f009972c312f74b
atomfaster@gmail.com <atomfaster@gmail.com>
2023-04-27 19:26:34 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
   <NavBar />
   <router-view />
   <hr>
-  <Footer />
+  <FooterVue />
 </template>
 
 <script>
 import axios from 'axios'
 import AuthService from '@/services/AuthService.js'
 import Home from '@/views/Home.vue'
-import NavBar from '@/components/NavBar.vue'
-import Footer from '@/components/Footer.vue'
+import NavBar from './components/NavBar.vue'
+import FooterVue from './components/Footer.vue'
 
 export default {
   name: 'App',
-  components: {NavBar, Footer},
+  components: {NavBar, FooterVue},
   beforeCreate() {
     this.$store.commit('initializeStore')
     const token = this.$store.state.token
