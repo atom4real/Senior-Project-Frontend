ccfa2230eb27226b383de083c797076687424eb1
atomfaster@gmail.com <atomfaster@gmail.com>
2023-05-03 01:53:44 +0700

    change baseURL

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import axios from 'axios'
 import VeeValidate from 'vee-validate';
 
-axios.defaults.baseURL = "http://127.0.0.1:8000","http://localhost:8000"
+axios.defaults.baseURL = "http://127.0.0.1:8000","http://localhost:8000","http://54.169.123.14:8000"
 
 // const app = createApp(App)
 // app.use(router).use(axios).mount('#app') // .provide('store', store)
